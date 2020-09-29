3b4a66f62d28fc97fef228b8c0e5b2b62e64c6ab
Luis Torres <luistjr@gmail.com>
2020-09-29 18:58:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -15,6 +15,6 @@ def find_min_in_nested_arrays(src)
     if src[row_index][element_index] < min
       min = src[row_index][element_index]
     end 
-    element_index
+    element_index += 1 
 
 end
